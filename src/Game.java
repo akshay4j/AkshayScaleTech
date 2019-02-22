6f1cbeac1ebbf4dbffd12257015f09e0b674320d
aky98 <33903437+aky98@users.noreply.github.com>
2019-02-22 15:13:16 +0530

    comments, notes added

--- src/Game.java
+++ src/Game.java
@@ -2,39 +2,49 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
+	
 	final int array[] = { 1, 0, 2, 0, 3, 0, 4, 0, 5, 0 };
+
+	static Game game;
+	Scanner scan;
+	
+	//game meta data
 	int size;
 	long scorePoints;// score
 	long bets;// number of bets played
-	static Game game;
-	Scanner scan;
 	int betPoints;// amout to make a bet
 	int mul;
 
 	private Game() {
+		//initilizing creds values
 		this.size = array.length;
 		this.scorePoints = 1000;
 		this.scan = new Scanner(System.in);
 		this.betPoints = 10;
 		this.bets = 0;
+	
+		
 		System.out.print("Enter Multiplier: ");
 		mul = scan.nextInt();
 	}
 
 	public static void main(String as[]) {
 		game = new Game();
+		
 		while (true) {
+			
 			System.out.println("<<< Choose Option >>>");
 			System.out.println("1 : for Bet || 0 : for Exit");
 			int option = game.scan.nextInt();
+			
 			switch (option) {
-			case 1: {
+			case 1: {//to play a game bets
 				System.out.println("How many Bets ??");
 				long betsToDo = game.scan.nextLong();
 				game.makeMultipleBets(betsToDo);
 				break;
 			}
-			case 0: {
+			case 0: {//exit way
 				game.endGame();
 				break;
 			}
@@ -47,8 +57,10 @@ public class Game {
 		}
 	}
 
+	//makes single bet 
+	//deduct amount, generates random, multiplies and credits to score 
 	private void makeBet() {
-		if (game.scorePoints >= game.betPoints) {
+		if (game.scorePoints >= game.betPoints) {//enought points to make bet
 			game.scorePoints -= game.betPoints;
 
 			int random = game.getRandom();
@@ -58,31 +70,34 @@ public class Game {
 			game.bets++;
 			game.showBetResult(result, random);
 
-		} else {
+		} else {//not enought points , exit the game
 			System.out.println("not enought points to make a bet.");
 			game.endGame();
 		}
 	}
 
-	private void showBetResult(long result, int random) {
+	private void showBetResult(long result, int random) {//shows a single bet result, then continue to game
 		System.out.println("Random number choosen: " + random);
 		System.out.println("Bet Amount: "+betPoints+" || " +"Points Made: "+result +" || "+ "Credit : " + (result - betPoints));
 		System.out.println("Total Score Points: " + game.scorePoints+" || " +" Total Bets: "+game.bets);
 		System.out.println();
 	}
 
-	private void makeMultipleBets(long no) {
+	private void makeMultipleBets(long no) {// to perform multiple bets
+		//performs single bet n times
 		for (long i =0; i < no; i++) {
 			game.makeBet();
 		}
 	}
 
+	//shows final game meta data and exits runtime
 	private void endGame() {
 		System.out.println("total bets: " + game.bets);
 		System.out.println("Final Score: " + game.scorePoints);
 		System.exit(0);
 	}
 
+	//picks random entry from array
 	private int getRandom() {
 		return new Random().nextInt(game.size);
 	}
--- src/LRUCache.java
+++ src/LRUCache.java
@@ -2,64 +2,77 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class LRUCache {
+	//least recently used means so far in terms of time used value will be removed first
 
+	//these 3 vars we need at anytime of runtime , so these should be static 
 	static String memory[];
 	static Scanner scan = new Scanner(System.in);
 	static int size = 0;
 
 	public static void main(String as[]) throws Exception{
-		Scanner scan = new Scanner(System.in);
-
+		//to deside memory size
+		//java array is used as memory
+		
+		//first will be most recently used
+		// MRU --> [ ] [ ] [ ] [ ] [ ] [ ] <-- LRU
+		//								last will be most recently used...
+		//MRU to LRU sorting order will be followed, timestamp not logged
+		
 		System.out.println("enter memory size(blocks): ");
 		size = scan.nextInt();
 		scan.nextLine();
-
+		
 		memory = new String[size];
 
+		//infinite loop for cxontinuous execution , exit condition impl.ed
 		while (true) {
 			LRUCache lru = new LRUCache();
 			String in;
 			lru.help();
 			System.out.println("Enter Command: ");
+			//input stream will be stored on string
+			//command pettern>> { [command] [data] } // ex: "add abc" , "remove abc" , "exit"
 
 			in = scan.nextLine();
+			//split from 0 to 1st space (it must be command token)
 			String[] command = in.split(" ");
-
+			
+			//check if its not blank (it will never be a space, we have trimmed)
 			if (command[0] != null && !command[0].trim().equals("")) {
-
+				
 				switch ((String) command[0].trim().toLowerCase()) {
-				case "exit": {
+				case "exit": {//exit command
 					lru.printMemory();
 					scan.close();
 					System.exit(0);
 				}
-				case "add": {
-					if (in.length() > 4) {
+				case "add": {//add command, to add single value
+					if (in.length() > 4) {//if length is less, syntex error
 						lru.add(in.substring(4));
 					}
 					lru.printMemory();
 					continue;
 				}
-				case "remove": {
+				case "remove": {//to remove entry if its avail
 					lru.remove(in, lru);
 					lru.printMemory();
 					continue;
 				}
-				case "fatch": {
+				case "fatch": {//fatch if avail, and set it as recently used
 					lru.fatch(in, lru);
 					lru.printMemory();
 					continue;
 				}
-				case "print": {
+				case "print": {//printys memory
 					lru.printMemory();
 					continue;
 				}
-				case "help": {
+				case "help": {//instruction set
 					lru.help();
 					continue;
 				}
 				// command for testing purpose
-				case "par": {
+				case "par": {//push all to right side, blank nodes in array in between, order will remain as it is
 					lru.pushAllRight();
 					lru.printMemory();
 					continue;
@@ -75,6 +88,8 @@ public class LRUCache {
 		} // eof loop
 	}///// end of main , eof program
 
+	
+	//instruction set
 	private void help() {
 		System.out.println();
 		System.out.println("here are the following commands to make operations:");
@@ -85,6 +100,21 @@ public class LRUCache {
 		System.out.println("5) to exit program : \"exit\" ");
 		System.out.println("6) to get command instructions : \"help\" ");
 		System.out.println();
+	}
+	
+	//to add a new entry,
+//	it will be at zero position
+//	if 0 is not null, then push all right side, and check again if 0 = null 
+//	if not means memory is full >> remove LRU(last node=null)
+	private void add(String add) {
+
+		pushAllRight();
+		if (memory[0] == null) {
+			memory[0] = add;
+		} else {// all full , remove 1
+			oneStepBack();
+			memory[0] = add;
+		}
 	}
 
 	private void fatch(String in, LRUCache lru) {
@@ -121,16 +151,7 @@ public class LRUCache {
 
 //			most ->  [ 0 ]  [ 1 ]  [ 2 ]  [ 3 ]  [ 4 ]  <-least
 
-	private void add(String add) {
-
-		pushAllRight();
-		if (memory[0] == null) {
-			memory[0] = add;
-		} else {// all full , remove 1
-			oneStepBack();
-			memory[0] = add;
-		}
-	}
+	
 
 	private void pushAllRight() {
 
